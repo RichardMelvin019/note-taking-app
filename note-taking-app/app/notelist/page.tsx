afa60f0f4fa76d744c130624b1516d7658f733b6
RichardMelvin019 <richardmelvin019@gmail.com>
2024-07-03 01:23:56 +0100

    successfully implemented the deleteNote fuction

--- note-taking-app/app/notelist/page.tsx
+++ note-taking-app/app/notelist/page.tsx
@@ -2,25 +2,31 @@
 
 import { HiStar, HiTrash, HiPencilSquare } from "react-icons/hi2";
 
-const NoteList = ({ notes }) => {
+const NoteList = ({ notes, onDeleteNote }) => {
   return (
     <div className="grid grid-cols-3 gap-4 justify-normal">
       {notes.map((note) => (
         <div className="items-center border-double border-4 border-primary p-6 rounded-lg">
           <HiStar />
           <div className="flex flex-row m-3 space-x-44">
-            <strong>TITLE</strong>
-            <button className="btn btn-ghost">
+            <strong>{note.title}</strong>
+            <button className="btn btn-ghost" onClick={() => onDeleteNote(note.id)}>
               <HiTrash className="text-red-600" />
             </button>
           </div>
           <div className="flex flex-row m-1 space-x-28">
-            <p>Note Description</p>
+            <p>{note.description}</p>
             <button className="btn btn-ghost">
               <HiPencilSquare className="text-blue-600" />
             </button>
           </div>
-          <small>Last modified [date]</small>
+          <small>{new Date(note.lastModified).toLocaleDateString("en-US", {
+            hour: "2-digit",
+            minute: "2-digit",
+            year: "numeric",
+            month: "2-digit",
+            day: "2-digit"
+          })}</small>
         </div>
       ))}
     </div>
--- note-taking-app/app/page.tsx
+++ note-taking-app/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
   const onAddNote = () => {
     const newNote = {
       id: uuid(), // Creates a random unique id.
-      title: "",
+      title: "Untitled",
       description: "",
       lastModified: Date.now(),
     };
@@ -27,13 +27,20 @@ export default function Home() {
     setNotes([newNote, ...notes]); // Adds the new note to the notes array (By spreading the elements inside notes).
   };
 
+  const onDeleteNote  = (idToDelete: string) => {
+    // Filters out the note with the provided id.
+    // (only if the logic passes true will the item be able to stay but if false the item will be removed)
+    const deleteNotes = notes.filter((note) => note.id !== idToDelete);
+    setNotes(deleteNotes); // Updates the notes array.
+  }
+
   return (
     <main className="flex flex-col items-center m-20">
       <div className="font-mono">Note Taking App</div>
       <Note onAddNote={onAddNote} />
       <div className="divider divider-secondary mb-7">Notes</div>
       <div className="mt-5">
-        <NoteList notes={notes} />
+        <NoteList notes={notes} onDeleteNote={onDeleteNote} />
       </div>
     </main>
   );
